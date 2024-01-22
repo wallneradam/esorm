350272551a2f4b212b3b3510233cedb53c02bbce
Adam Wallner <adam.wallner@gmail.com>
2024-01-22 18:32:56 +0100

    Refactored aggregation TypeDicts

--- esorm/aggs.py
+++ esorm/aggs.py
@@ -9,7 +9,7 @@ from typing_extensions import TypeAlias
 # Queries
 #
 
-class ESTermsAgg(TypedDict, total=False):
+class ESAggTermParams(TypedDict, total=False):
     """
     Represents the parameters for a terms aggregation in Elasticsearch.
     """
@@ -21,39 +21,15 @@ class ESTermsAgg(TypedDict, total=False):
     """ The order of the buckets. """
 
 
-class ESAvgAgg(TypedDict):
+class ESAggFieldParams(TypedDict):
     """
-    Represents the parameters for an average aggregation in Elasticsearch.
-    """
-    field: str
-    """ The field to aggregate on. """
-
-
-class ESSumAgg(TypedDict):
-    """
-    Represents the parameters for a sum aggregation in Elasticsearch.
+    Represents field parameter in Elasticsearch.
     """
     field: str
     """ The field to aggregate on. """
 
 
-class ESMinAgg(TypedDict):
-    """
-    Represents the parameters for a minimum aggregation in Elasticsearch.
-    """
-    field: str
-    """ The field to aggregate on. """
-
-
-class ESMaxAgg(TypedDict):
-    """
-    Represents the parameters for a maximum aggregation in Elasticsearch.
-    """
-    field: str
-    """ The field to aggregate on. """
-
-
-class ESExtendedBounds(TypedDict):
+class ESAggExtendedBounds(TypedDict):
     """
     Represents the parameters for extended bounds in Elasticsearch.
     """
@@ -63,7 +39,7 @@ class ESExtendedBounds(TypedDict):
     """ The maximum value. """
 
 
-class ESHistogramAgg(TypedDict, total=False):
+class ESAggHistogramParams(TypedDict, total=False):
     """
     Represents the parameters for a histogram aggregation in Elasticsearch.
     """
@@ -73,7 +49,7 @@ class ESHistogramAgg(TypedDict, total=False):
     """ The interval of the histogram. """
     min_doc_count: int
     """ The minimum number of documents in a bucket. """
-    extended_bounds: ESExtendedBounds
+    extended_bounds: ESAggExtendedBounds
     """ The extended bounds of the histogram. """
 
 
@@ -81,17 +57,17 @@ class ESAgg(TypedDict, total=False):
     """
     Holds all types of aggregations supported
     """
-    terms: ESTermsAgg
+    terms: ESAggTermParams
     """ Terms aggregation """
-    avg: ESAvgAgg
+    avg: ESAggFieldParams
     """ Average aggregation """
-    sum: ESSumAgg
+    sum: ESAggFieldParams
     """ Sum aggregation """
-    min: ESMinAgg
+    min: ESAggFieldParams
     """ Minimum aggregation """
-    max: ESMaxAgg
+    max: ESAggFieldParams
     """ Maximum aggregation """
-    histogram: ESHistogramAgg
+    histogram: ESAggHistogramParams
     """ Histogram aggregation """
 
 
@@ -103,7 +79,7 @@ ESAggs: TypeAlias = Dict[str, ESAgg]
 # Responses
 #
 
-class ESBucket(TypedDict):
+class ESAggBucketResponse(TypedDict):
     """
     Represents a single bucket in a bucket aggregation.
     """
@@ -113,15 +89,15 @@ class ESBucket(TypedDict):
     """ The number of documents in this bucket. """
 
 
-class ESTermsAggResponse(TypedDict):
+class ESAggTermsResponse(TypedDict):
     """
     Represents the response for a terms aggregation.
     """
-    buckets: List[ESBucket]
+    buckets: List[ESAggBucketResponse]
     """ A list of buckets in the terms aggregation. """
 
 
-class ESAvgMinMaxAggResponse(TypedDict):
+class ESAggValueResponse(TypedDict):
     """
     Represents the response for an average, min, or max aggregation.
     """
@@ -129,7 +105,7 @@ class ESAvgMinMaxAggResponse(TypedDict):
     """ The average, min, or max value. """
 
 
-class ESHistogramBucket(TypedDict):
+class ESAggHistogramBucketresponse(TypedDict):
     """
     Represents a bucket in a histogram aggregation.
     """
@@ -139,17 +115,17 @@ class ESHistogramBucket(TypedDict):
     """ The number of documents in this bucket."""
 
 
-class ESHistogramAggResponse(TypedDict):
+class ESAggHistogramResponse(TypedDict):
     """
     Represents the response for a histogram aggregation.
     """
-    buckets: List[ESHistogramBucket]
+    buckets: List[ESAggHistogramBucketresponse]
     """ A list of buckets in the histogram aggregation. """
 
 
 ESAggsResponse: TypeAlias = Dict[str, Union[
-    ESTermsAggResponse,
-    ESAvgMinMaxAggResponse,
-    ESHistogramAggResponse
+    ESAggValueResponse,
+    ESAggTermsResponse,
+    ESAggHistogramResponse
 ]]
 """ ElasticSearch aggregations response type definition """
